a8f31a29fc6eb1221d67a5e20a81ef2a30f9363e
Tad <tadevosian.sarkis@gmail.com>
2023-05-09 14:45:29 +0200

    almost done

--- utils/game.py
+++ utils/game.py
@@ -1,48 +1,54 @@
-class hangman:
+class Hangman:
     def __init__(self, word):
         self.word = word
-        self.possible_words = ['rambunctious','serendipity','miscellaneous','becode', 'learning', 'mathematics', 'sessions']
+        self.possible_words = ['sunflower', 'pho','rambunctious','serendipity','miscellaneous','becode', 'learning', 'mathematics', 'sessions']
         self.word_to_find = list(word)
         self.lives = 5
-        self.correctly_guessed_letters = ["_"] * len(self.word)
-
+        self.correctly_guessed_letters = ["_"] * len(word)
+        self.wrongly_guessed_letters = []
+        """
+        - A `wrongly_guessed_letters` attribute that contains a list of strings where each 
+        element will be a letter guessed by the user that is not in the `word_to_find`.
+        """
+        self.turn_count= 0
+        self.error_count = 0
      
-        def guess_letter(self, letter):
-            for i in range(len(self.word_to_find)):
-                if self.word_to_find[i] == letter:
-                    self.correctly_guessed_letters[i] = letter
+        def play():
+            guess = input("Enter a letter: ").upper()
+            while self.lives > 0:
+                guess = input("Enter a letter: ").upper()
+                if guess in self.word_to_find:
+                    print(f"{guess} is in the word!")
+                    self.correctly_guessed_letters.append(guess)
+                    self.turn_count += 1
 
+                elif guess not in self.word_to_find:
+                    print(f"{guess} is not in the word!")
+                    self.wrongly_guessed_letters.append(guess)
+                    self.lives -= 1
+                    self.error_count += 1
+                    self.turn_count += 1
+                    
+                elif len(guess) > 1 or guess != guess.isalpha():
+                    print(f"{guess} is not a valid letter !")
+                
+                print(self.correctly_guessed_letters, self.wrongly_guessed_letters, self.lives,self.error_count, self.turn_count)
+
+                
+
+
+        def start_game():
+            if lives == 0:
+                print("Game over!")
         """
-        self.guessed_word = []
-        self.guessed_word_count = 0
-        self.word_length = len(self.word)
-        self.guessed_word_length = len(self.guessed_word)
-        self.guessed_letters_length = len(self.guessed_letters)
-        self.guessed_letters_count = self.guessed_letters_length
-        self.guessed_word_count = self.guessed_word_length
+            - will call `play()` until the game is over (because the use guessed the word or because of a game over).
+            - will call `game_over()` if `lives` is equal to 0.
+            - will call `well_played()` if all the letter are guessed.
+            - will print `correctly_guessed_letters`, `bad_guessed_letters`, `life`, `error_count` and `turn_count` at the end of each turn.
         """
-
-"""
-- A `wrongly_guessed_letters` attribute that contains a list of strings where each 
-element will be a letter guessed by the user that is not in the `word_to_find`.
-- A `turn_count` attribute that contain the number of turns played by the player. 
-This will be represented as an `int`.
-
-- An `error_count` attribute that contains the number of errors made by the player.
-
-- A `play()` method that asks the player to enter a letter. 
-Be careful that the player shouldn't be allowed to type something else than a letter, and not more than a letter. 
-If the player guessed a letter well, add it to the `correctly_guessed_letters` list. 
-If not, add it to the `wrongly_guessed_letters` list and add 1 to `error_count`.
-
-- A `start_game()` method that:
-  - will call `play()` until the game is over (because the use guessed the word or because of a game over).
-  - will call `game_over()` if `lives` is equal to 0.
-  - will call `well_played()` if all the letter are guessed.
-  - will print `correctly_guessed_letters`, `bad_guessed_letters`, `life`, `error_count` and `turn_count` at the end of each turn.
-
-- A `game_over()` method that will stop the game and print `game over...`.
-
-- A `well played()` method that will print `You found the word: {word_to_find_here} 
-in {turn_count_here} turns with {error_count_here} errors!`.
-"""
+        def game_over():
+            print("game over...")
+        
+        def well_played():
+            print(f"You found the word: {word_to_find}", 
+                  "in {turn_count} turns with {error_count} errors!")
